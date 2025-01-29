3e2c556009dc17fb5e807980ad3fab933d371709
Mathias JP <175939829+Jaegermeisty@users.noreply.github.com>
2025-01-29 14:55:43 +0100

    Create human and computer choice function

---
+++ index.js
@@ -0,0 +1,21 @@
+function getComputerChoice() {
+  let number = Math.random();
+  let computerChoice = '';
+  
+  if (number <= 0.333) {
+    computerChoice = 'Rock';
+  } else if (number > 0.333 && number <= 0.666) {
+    computerChoice = 'Paper';
+  } else {
+    computerChoice = 'Scissor';
+  }
+
+  return computerChoice;
+}
+
+function getHumanChoice() {
+  answer = prompt('Choose between rock, paper, scissor');
+  let humanChoice = answer.charAt(0).toUpperCase() + answer.slice(1).toLowerCase();
+  
+  return humanChoice;
+}
